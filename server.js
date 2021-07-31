e4566f2a4ee4c08a195fca8e5ea7de3da388df82
Minh Le Ngoc <minhle@Mac-mini.local>
2021-07-31 13:33:05 +0700

    add log

--- server.js
+++ server.js
@@ -9,19 +9,24 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/api/link-preview', async (req, res) => {
-  const { content } = req.query;
-  console.log(content);
+  try {
+    const { content } = req.query;
+    console.log(content);
 
-  const result = await getLinkPreview(content, {
-    imagesPropertyType: 'og', // fetches only open-graph images
-    headers: {
-      'user-agent': 'googlebot', // fetches with googlebot crawler user agent
-    },
-    timeout: 1000,
-  });
-  console.log('getLinkPreview', result);
+    const result = await getLinkPreview(content, {
+      imagesPropertyType: 'og', // fetches only open-graph images
+      headers: {
+        'user-agent': 'googlebot', // fetches with googlebot crawler user agent
+      },
+      timeout: 1000,
+    });
+    console.log('getLinkPreview', result);
 
-  res.send(result);
+    res.send(result);
+  } catch (error) {
+    console.log('error');
+    res.send(error);
+  }
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
