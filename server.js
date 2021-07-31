38ea3d32ee68877c0bafdfa21dbc39beea646696
Minh Le Ngoc <minhle@Mac-mini.local>
2021-07-31 13:50:20 +0700

    enable cors

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const express = require('express');
-const { getLinkPreview, getPreviewFromContent } = require('link-preview-js');
 const bodyParser = require('body-parser');
+var cors = require('cors');
+
+const { getLinkPreview, getPreviewFromContent } = require('link-preview-js');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
